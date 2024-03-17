41e97435664bc5b1b7b50dea8fd28faf843746d1
laduniTI <160467095+laduniTI@users.noreply.github.com>
2024-03-18 04:41:28 +0700

    coba

--- app/src/main/java/com/example/appkotlin/Aquarium.kt
+++ app/src/main/java/com/example/appkotlin/Aquarium.kt
@@ -1,29 +1,10 @@
-package com.example.appkotlin
+class Aquarium(var width: Int = 20, var height: Int = 40, var length: Int = 100) {
+    fun volume() = width * height * length / 1000
+    fun waterNeeded() = volume() * 0.9
+}
 
-class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
-    // Dimensions in cm
-    var length: Int = length
-    var width: Int = width
-    var height: Int = height
-    fun printSize() {
-        println("Width: $width cm " +
-                "Length: $length cm " +
-                "Height: $height cm ")
-    }
-}
-fun buildAquarium() {
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
-    // default height and length
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-    // default width
-    val aquarium3 = Aquarium(height = 35, length = 100)
-    aquarium3.printSize()
-    // everything custom
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 100)
-    aquarium4.printSize()
-}
 fun main() {
-    buildAquarium()
+    val myAquarium = Aquarium()
+    println("Volume: ${myAquarium.volume()} liter")
+    println("Air yang dibutuhkan: ${myAquarium.waterNeeded()} liter")
 }
