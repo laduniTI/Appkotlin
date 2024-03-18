d1e8b12a86a5f8878f7b9b5097f9e00d4ecfe4ff
laduniTI <160467095+laduniTI@users.noreply.github.com>
2024-03-18 10:08:47 +0700

    coba

--- app/src/main/java/com/example/appkotlin/fungsibuku.kt
+++ app/src/main/java/com/example/appkotlin/fungsibuku.kt
@@ -1,2 +1,24 @@
 package com.example.appkotlin
 
+class Buku(
+    val judul: String,
+    val penulis: String,
+    val tahunTerbit: Int,
+    val harga: Double) {
+    // Properties atau atribut lainnya bisa ditambahkan sesuai kebutuhan, seperti ISBN, jumlah halaman, dll.
+
+    // Fungsi untuk menampilkan informasi buku
+    fun tampilkanInformasi() {
+        println("Judul: $judul")
+        println("Penulis: $penulis")
+        println("Tahun Terbit: $tahunTerbit")
+        println("Harga: Rp. $harga")
+    }
+}
+
+fun main() {
+    // Membuat objek buku
+    val buku1 = Buku("cerita anak desa", "andika", 1997, 150000.0)
+    buku1.tampilkanInformasi()
+}
+
--- app/src/main/java/com/example/appkotlin/main/AQuarium.kt
+++ app/src/main/java/com/example/appkotlin/main/AQuarium.kt
@@ -1,2 +1,10 @@
-package com.example.appkotlin.main
+class Aquarium(var width: Int = 20, var height: Int = 40, var length: Int = 100) {
+    fun volume() = width * height * length / 1000
+    fun waterNeeded() = volume() * 0.9
+}
 
+fun main() {
+    val myAquarium = Aquarium()
+    println("Volume: ${myAquarium.volume()} liter")
+    println("Air yang dibutuhkan: ${myAquarium.waterNeeded()} liter")
+}
